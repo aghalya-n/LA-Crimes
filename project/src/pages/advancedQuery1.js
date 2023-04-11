08cef4a2fcec40f2b06c0595190a6f390e62cf88
Sailaja <90356296+snall6@users.noreply.github.com>
2023-04-11 01:36:27 -0500

    Update and rename page2.js to advancedQuery1.js

---
+++ project/src/pages/advancedQuery1.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './advancedQuery1.css'
+
+const AdvancedQuery1 = () => {
+  const [res, setRes] = useState([])
+  
+  const handleClick = async () => {
+    try {
+      const response = await axios({
+        url: 'http://127.0.0.1:5000/',
+        method: 'GET',
+        params: {
+        AreaName: "xyz"
+        },
+      });
+
+      const data = response.data
+      console.log(data);
+      setRes(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  return (
+    <div>
+
+      <Link to="/">
+        <button className="return-button" >Home</button>
+      </Link>
+      <Link to="/advancedQuery2">
+        <button className="aq2-button" >Even More Info!</button>
+    </Link>
+      <Center><h1>See the number of ongoing crime investigations by crime type below!</h1></Center>
+      <Center><button className="enter" onClick={handleClick}>Submit</button></Center>
+      <Center><div className="output">
+      <table>
+        <tr>
+           <td>Crime Description</td>
+           <td>Num Open Investigations</td>
+        </tr>
+        {res.map((item) => (
+  <tr>
+    {item.map((d) => (
+      <td>{d}</td>
+    ))}
+  </tr>
+))}
+       </table>
+    </div></Center>
+    </div>
+  );
+};
+  
+export default AdvancedQuery1;
+
+
+function Center({children}) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
